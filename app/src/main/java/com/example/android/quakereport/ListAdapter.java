5e3c8f366ea4f3128425996b8a010c064eeaff33
Resul <resulsaparov@gmail.com>
2018-11-23 00:27:33 +0300

    Better UI JSON info added

--- app/src/main/java/com/example/android/quakereport/ListAdapter.java
+++ app/src/main/java/com/example/android/quakereport/ListAdapter.java
@@ -1,15 +1,20 @@
 package com.example.android.quakereport;
 
 import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class ListAdapter extends ArrayAdapter<Quake>
 {
@@ -31,15 +36,72 @@ public class ListAdapter extends ArrayAdapter<Quake>
 
         final Quake quake = getItem(position);
 
+        String locCity[] = new String[2];
+        String loc = quake.getCity();
+
+        if (loc.contains("of"))
+        {
+            int n = loc.indexOf("of") + 3;
+            locCity[0] = loc.substring(0, n);
+            locCity[1] = loc.substring(n);
+        }
+        else
+        {
+            locCity[0] = "Near the ";
+            locCity[1] = loc;
+        }
+
+        TextView txtLocation = listView.findViewById(R.id.txtLocation);
+        txtLocation.setText(locCity[0]);
+
         TextView txtCity = listView.findViewById(R.id.txtCity);
-        txtCity.setText(quake.getCity());
+        txtCity.setText(locCity[1]);
+
+        DecimalFormat formatter = new DecimalFormat("0.0");
+
 
         TextView txtMagnitude = listView.findViewById(R.id.txtMagnitude);
-        txtMagnitude.setText(String.valueOf(quake.getMagnitude()));
+        txtMagnitude.setText(String.valueOf(formatter.format(quake.getMagnitude())));
+
+        Date date = new Date(Long.parseLong(quake.getDate()));
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MMM dd, yyyy");
 
         TextView txtDate = listView.findViewById(R.id.txtDate);
-        txtDate.setText(quake.getDate());
+        txtDate.setText(dateFormat.format(date));
+
+        dateFormat = new SimpleDateFormat("HH:mm");
+
+        TextView txtTime = listView.findViewById(R.id.txtTime);
+        txtTime.setText(dateFormat.format(date));
+
+        GradientDrawable magnitudeCircle = (GradientDrawable) txtMagnitude.getBackground();
+
+        // Get the appropriate background color based on the current earthquake magnitude
+        int magnitudeColor = getMagnitudeColor(quake.getMagnitude());
+
+        // Set the color on the magnitude circle
+        magnitudeCircle.setColor(magnitudeColor);
 
         return listView;
+    }
+
+    private int getMagnitudeColor(double magnitude)
+    {
+        int i;
+        switch ((int)magnitude)
+        {
+            case 0:
+            case 1: i = R.color.magnitude1; break;
+            case 2: i = R.color.magnitude2; break;
+            case 3: i = R.color.magnitude3; break;
+            case 4: i = R.color.magnitude4; break;
+            case 5: i = R.color.magnitude5; break;
+            case 6: i = R.color.magnitude6; break;
+            case 7: i = R.color.magnitude7; break;
+            case 8: i = R.color.magnitude8; break;
+            case 9: i = R.color.magnitude9; break;
+            default: i = R.color.magnitude10plus;
+        }
+        return ContextCompat.getColor(getContext(), i);
     }
 }
--- app/src/main/java/com/example/android/quakereport/MainActivity.java
+++ app/src/main/java/com/example/android/quakereport/MainActivity.java
@@ -18,15 +18,7 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
 
         // Create a fake list of earthquake locations.
-        ArrayList<Quake> earthquakes = new ArrayList<>();
-
-        earthquakes.add(new Quake(7.2, "San Francisco", "Feb 2, 2016"));
-        earthquakes.add(new Quake(6.1, "London", "July 2, 2016"));
-        earthquakes.add(new Quake(3.9, "Tokyo", "Nov 2, 2016"));
-        earthquakes.add(new Quake(5.4, "Mexico City", "May 2, 2016"));
-        earthquakes.add(new Quake(2.8, "Moscow", "Jan 2, 2016"));
-        earthquakes.add(new Quake(4.9, "Rio de Janeiro", "Aug 2, 2016"));
-        earthquakes.add(new Quake(1.6, "Paris", "Oct 2, 2016"));
+        ArrayList<Quake> earthquakes = QueryUtils.extractEarthquakes();
 
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = findViewById(R.id.list);
